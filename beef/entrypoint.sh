461667cc15b2000031f597c83de3cea939357d8f
Marc Aldrich <marcaldrich@gmail.com>
2021-03-03 19:36:12 -0600

    Beef: updated user contexts.
    beef: testing build context

--- beef/entrypoint.sh
+++ beef/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cd /home/beef
+cd /opt/beef
 test -z "$BEEF_USER" || sed -e '/user:/s/"beef"/"'"$BEEF_USER"'"/' -i config.yaml
 test -z "$BEEF_PASSWORD" || sed -e '/passwd:/s/"beef"/"'"$BEEF_PASSWORD"'"/' -i config.yaml
 exec ./beef "$@"
